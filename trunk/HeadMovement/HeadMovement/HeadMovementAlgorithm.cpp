8e580d39f576f6df9baa57f77f0231f953fdc408
bkornel@gmail.com <bkornel@gmail.com@fc0fb0cc-d1a1-fc30-3b48-6878658956fa>
2012-10-12 18:34:38 +0000

    HeadMovement update

--- trunk/HeadMovement/HeadMovement/HeadMovementAlgorithm.cpp
+++ trunk/HeadMovement/HeadMovement/HeadMovementAlgorithm.cpp
@@ -1,4 +1,5 @@
 #include "HeadMovementAlgorithm.h"
+#include "HaarDetector.h"
 
 using namespace std;
 using namespace cv;
@@ -10,11 +11,13 @@ HeadMovementAlgorithm::HeadMovementAlgorithm(void)
 	// check if we succeeded
 	if(!videoCapture_.isOpened())
 		CV_Error(1, "VideoCapture is not opened!");
-}
 
+	faceDetector_ = new HaarDetector("FaceDetector");
+}
 
 HeadMovementAlgorithm::~HeadMovementAlgorithm(void)
 {
+	delete faceDetector_;
 }
 
 void HeadMovementAlgorithm::ProcessAlgorithm(void)
@@ -26,11 +29,46 @@ void HeadMovementAlgorithm::ProcessAlgorithm(void)
 		// get a new frame from camera
 		videoCapture_ >> frame_;
 
-		// displays the new frame
-		imshow("HeadMovementAlgorithm", frame_);
+		// detects faces on the incoming frames
+		faces_ = faceDetector_->Detect(frame_);
+
+		if(prevFrame_.empty() == false)
+		{
+			Mat subFrame;
+
+			absdiff(frame_, prevFrame_, subFrame);
+			DrawObjects(frame_, faces_);
+
+			// displays the new frame
+			imshow("HeadMovementAlgorithm", subFrame);
+		}
+		
+		prevFrame_ = frame_.clone();
 
 		// press ESC to exit
 		if(waitKey(30) >= 0) 
 			break;
+	}
+}
+
+void HeadMovementAlgorithm::DrawObjects(Mat& image, const vector<Rect>& objects)
+{
+	int i = 0;
+	const static Scalar colors[8] =  { 
+		CV_RGB(0 ,0, 255),
+		CV_RGB(0, 128, 255),
+		CV_RGB(0, 255, 255),
+		CV_RGB(0, 255, 0),
+		CV_RGB(255, 128, 0),
+		CV_RGB(255, 255, 0),
+		CV_RGB(255, 0, 0),
+		CV_RGB(255, 0, 255)
+	};
+
+	for(vector<Rect>::const_iterator r = objects.begin(); r != objects.end(); r++, i++)
+	{
+		Point center(cvRound(r->x + r->width * 0.5), cvRound(r->y + r->height * 0.5));
+		int radius = cvRound((r->width + r->height) * 0.25);
+		circle(image, center, radius, colors[i % 8], 3);
 	}
 }
