645e89ca266fe1584ca94ed1e8d357e1f94b121d
bkornel <bkornel@gmail.com>
2012-10-12 18:35:42 +0000

    add missing files

---
+++ HeadMovement/ImageProcessing/Definitions.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+#define DEBUG_MODE 1
+#define RESOURCES_PATH "../resources/"
+#define HAAR_CASCADE_PATH (string(RESOURCES_PATH) + "haarCascades/")
+#define FACE_CASCADE (string(HAAR_CASCADE_PATH) + "haarcascade_frontalface_alt.xml")
