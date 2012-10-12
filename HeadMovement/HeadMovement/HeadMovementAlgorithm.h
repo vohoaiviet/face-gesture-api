540044a98c14b6879884aface476148cef973776
bkornel <bkornel@gmail.com>
2012-10-12 18:34:38 +0000

    HeadMovement update

--- HeadMovement/HeadMovement/HeadMovementAlgorithm.h
+++ HeadMovement/HeadMovement/HeadMovementAlgorithm.h
@@ -3,7 +3,9 @@
 
 #include "opencv2/opencv.hpp"
 
-extern "C++" class __declspec(dllexport) HeadMovementAlgorithm
+class HaarDetector;
+
+class HeadMovementAlgorithm
 {
 public:
 	HeadMovementAlgorithm(void);
@@ -12,6 +14,12 @@ public:
 	void ProcessAlgorithm(void);
 
 private:
+	void DrawObjects(cv::Mat& image, const std::vector<cv::Rect>& objects);
+
+	std::vector<cv::Rect> faces_;
+
+	HaarDetector* faceDetector_;
 	cv::VideoCapture videoCapture_;
 	cv::Mat frame_;
+	cv::Mat prevFrame_;
 };
