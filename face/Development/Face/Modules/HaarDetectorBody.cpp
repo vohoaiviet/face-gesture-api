999d9d67e62090c34f5b486a79337c0912570914
bkornel <bkornel@gmail.com>
2013-06-07 19:46:40 +0000

    LK

--- face/Development/Face/Modules/HaarDetectorBody.cpp
+++ face/Development/Face/Modules/HaarDetectorBody.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/video.hpp>
 
 #include "HaarDetectorBody.h"
 #include "Tracer.h"
@@ -14,8 +15,11 @@ using namespace std;
 HaarDetectorBody::HaarDetectorBody(const VertexElement& vertexElement)
 :   Body(vertexElement),
     imageWrapperIn_(NULL),
+	prevImageWrapperIn_(NULL),
     cascadeName_(""),
     scaleFactor_(1.2),
+	imgScaleFactor_(1.0),
+	invImgScaleFactor_(1.0),
     minNeighbors_(2),
     flags_(0),
     minSize_(Size()),
@@ -24,18 +28,44 @@ HaarDetectorBody::HaarDetectorBody(const VertexElement& vertexElement)
     const cv::FileStorage& configurationFs = GetModulConfigurationFs();
 
     if(!configurationFs["cascadeName"].empty() && configurationFs["cascadeName"].isString())
+	{
         configurationFs["cascadeName"] >> cascadeName_;
+	}
 
-    if(!configurationFs["scaleFactor"].empty() && configurationFs["scaleFactor"].isReal())
-        configurationFs["scaleFactor"] >> scaleFactor_;
+	if(!cascade_.load(GlobalSettingsConstPtr->GetDirectories().moduleSettings + GetModuleName() + "/" + cascadeName_))
+	{
+		CV_Error(-1, "Could not load cascade classifier: " + cascadeName_ + ".");
+	}
+
+	if(!configurationFs["scaleFactor"].empty() && configurationFs["scaleFactor"].isReal())
+	{
+		configurationFs["scaleFactor"] >> scaleFactor_;
+	}
+
+	if(!configurationFs["imgScaleFactor"].empty() && configurationFs["imgScaleFactor"].isReal())
+	{
+		configurationFs["imgScaleFactor"] >> imgScaleFactor_;
+
+		if(imgScaleFactor_ >= 1.0)
+			imgScaleFactor_ = 1.0;
+		else if(imgScaleFactor_ <= 0.0)
+			imgScaleFactor_ = 0.1;
+
+		invImgScaleFactor_ = 1.0 / imgScaleFactor_;
+	}
 
     if(!configurationFs["minNeighbors"].empty() && configurationFs["minNeighbors"].isInt())
+	{
         configurationFs["minNeighbors"] >> minNeighbors_;
+	}
 
     if(!configurationFs["flags"].empty() && configurationFs["flags"].isInt())
+	{
         configurationFs["flags"] >> flags_;
+	}
 
-    if(!configurationFs["minWidth"].empty() && !configurationFs["minHeight"].empty() && configurationFs["minWidth"].isInt() && configurationFs["minHeight"].isInt())
+    if(!configurationFs["minWidth"].empty() && !configurationFs["minHeight"].empty() && 
+		configurationFs["minWidth"].isInt() && configurationFs["minHeight"].isInt())
     {
         int minWidth, minHeight;
         configurationFs["minWidth"] >> minWidth;
@@ -43,38 +73,47 @@ HaarDetectorBody::HaarDetectorBody(const VertexElement& vertexElement)
         minSize_ = Size(minWidth, minHeight);
     }
 
-    if(!configurationFs["maxWidth"].empty() && !configurationFs["maxHeight"].empty() && configurationFs["maxWidth"].isInt() && configurationFs["maxHeight"].isInt())
+    if(!configurationFs["maxWidth"].empty() && !configurationFs["maxHeight"].empty() && 
+		configurationFs["maxWidth"].isInt() && configurationFs["maxHeight"].isInt())
     {
         int maxWidth, maxHeight;
         configurationFs["maxWidth"] >> maxWidth;
         configurationFs["maxHeight"] >> maxHeight;
         maxSize_ = Size(maxWidth, maxHeight);
     }
-
-    if(!cascade_.load(GlobalSettingsConstPtr->GetDirectories().moduleSettings + GetModuleName() + "/" + cascadeName_))
-        CV_Error(-1, "Could not load cascade classifier: " + cascadeName_ + ".");
 }
 
 
 HaarDetectorBody::HaarDetectorBody(const HaarDetectorBody& other)
 :   Body(other),
     imageWrapperIn_(NULL),
+	prevImageWrapperIn_(NULL),
     cascade_(other.cascade_),
     cascadeName_(other.cascadeName_),
     scaleFactor_(other.scaleFactor_),
+	imgScaleFactor_(other.imgScaleFactor_),
+	invImgScaleFactor_(other.invImgScaleFactor_),
     minNeighbors_(other.minNeighbors_),
     flags_(other.flags_),
     minSize_(other.minSize_),
-    maxSize_(other.maxSize_)
+    maxSize_(other.maxSize_),
+	objects_(other.objects_)
 {
-    if(other.imageWrapperIn_)
-        imageWrapperIn_ = new ImageWrapper(*other.imageWrapperIn_);
+	if(other.imageWrapperIn_)
+		imageWrapperIn_ = new ImageWrapper(*other.imageWrapperIn_);
+
+	if(other.prevImageWrapperIn_)
+		prevImageWrapperIn_ = new ImageWrapper(*other.prevImageWrapperIn_);
+
+	other.grayFrame_.copyTo(grayFrame_);
+	other.normalizedImage_.copyTo(normalizedImage_);
 }
 
 
 HaarDetectorBody::~HaarDetectorBody(void)
 {
     delete imageWrapperIn_;
+	delete prevImageWrapperIn_;
 }
 
 
@@ -82,11 +121,16 @@ Body::OutputType HaarDetectorBody::operator() (Body::InputType2 input)
 {
 	imageWrapperIn_ = dynamic_cast<ImageWrapper*>(std::get<INPUT_IMAGE>(input));
 
+	TRACE(GetFullName() + ": " + imageWrapperIn_->GetMetaData().GetFrameNumber());
+
 	BeforeProcess();
 	Process();
 	AfterProcess();
 
-	GarbageCollectorPtr->InputHasBeenProcessed(std::get<INPUT_IMAGE>(input));
+	if(prevImageWrapperIn_)
+		GarbageCollectorPtr->InputHasBeenProcessed(prevImageWrapperIn_);
+
+	prevImageWrapperIn_ = imageWrapperIn_;
 
     return output_;
 }
@@ -102,24 +146,79 @@ void HaarDetectorBody::operator= (const HaarDetectorBody& other)
 void HaarDetectorBody::Process(void)
 {
     const Mat& frameIn = imageWrapperIn_->Rgb();
-    Mat outputRgb = frameIn.clone();
+	const Mat& prevFrame = prevImageWrapperIn_->Rgb();
 
-    TRACE(GetFullName() + ": " + imageWrapperIn_->GetMetaData().GetFrameNumber());
+    outputFrame_ = frameIn.clone();
 
-    Mat grayFrame, normalizedImage;
+	Mat grayFrameRes(cvRound(frameIn.rows * imgScaleFactor_), cvRound(frameIn.cols * imgScaleFactor_), CV_8UC1);
+    cvtColor(frameIn, grayFrame_, CV_BGR2GRAY);
+	resize(grayFrame_, grayFrameRes, grayFrameRes.size());
+    equalizeHist(grayFrameRes, normalizedImage_);
 
-    cvtColor(frameIn, grayFrame, CV_BGR2GRAY);
-    equalizeHist(grayFrame, normalizedImage);
+	vector<Rect> prevObjects = objects_;
 
-    std::vector<cv::Rect> objects;
-    cascade_.detectMultiScale(normalizedImage, objects, scaleFactor_, minNeighbors_, flags_, minSize_, maxSize_);
+	if(objects_.empty() || imageWrapperIn_->GetMetaData().GetFrameNumber() % 5 == 0)
+	{
+		objects_.clear();
+		cascade_.detectMultiScale(normalizedImage_, objects_, scaleFactor_, minNeighbors_, flags_, minSize_, maxSize_);
 
-    if(!objects.empty())
-    {
-        Rect r = objects[0];
-        rectangle(outputRgb, Point(r.x, r.y), Point(r.x + r.width, r.y + r.height), Scalar(255, 0, 0), 2);
-    }
+		if(!objects_.empty())
+		{
+			for(vector<Rect>::iterator r = objects_.begin(); r != objects_.end(); r++)
+			{
+				r->x = cvRound(r->x * invImgScaleFactor_);
+				r->y = cvRound(r->y * invImgScaleFactor_);
+				r->width = cvRound(r->width * invImgScaleFactor_);
+				r->height = cvRound(r->height * invImgScaleFactor_);
 
-    IMSHOW(GetFullName(), outputRgb);
+				rectangle(outputFrame_, Point(r->x, r->y), Point(r->x + r->width, r->y + r->height), Scalar(255, 0, 0), 2);
+			}
+		}
+	}
+
+	else if(/*objects_.empty() &&*/ !prevObjects.empty() && !prevFrame.empty())
+	{
+		//objects_ = prevObjects;
+
+		vector<Point2f> prevPoints, nextPoints;
+		vector<Size> objSizes;
+		vector<uchar> status;
+		vector<float> err;
+		TermCriteria termcrit(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 20, 0.03);
+		Size winSize(5, 5);
+
+		Mat grayPrevFrame;
+		cvtColor(prevFrame, grayPrevFrame, CV_BGR2GRAY);
+
+		for(vector<Rect>::const_iterator r = prevObjects.begin(); r != prevObjects.end(); r++)
+		{
+			Point center(cvRound(r->x + r->width * 0.5), cvRound(r->y + r->height * 0.5));
+			prevPoints.push_back(center);
+			objSizes.push_back(Size(r->width, r->height));
+		}
+
+		calcOpticalFlowPyrLK(grayPrevFrame, grayFrame_, prevPoints, nextPoints, status, err, winSize, 3, termcrit, 0, 0.001);
+
+		if(prevPoints.size() == nextPoints.size())
+		{
+			for(size_t i = 0; i < nextPoints.size(); i++)
+			{
+				Point tl(cvRound(nextPoints[i].x - objSizes[i].width * 0.5), cvRound(nextPoints[i].y - objSizes[i].height * 0.5));
+				Point br(cvRound(nextPoints[i].x + objSizes[i].width * 0.5), cvRound(nextPoints[i].y + objSizes[i].height * 0.5));
+				objects_[i] = Rect(tl, br);
+			}
+		}
+		else
+		{
+			objects_.clear();
+		}
+
+		for(vector<Rect>::iterator r = objects_.begin(); r != objects_.end(); r++)
+		{
+			rectangle(outputFrame_, Point(r->x, r->y), Point(r->x + r->width, r->y + r->height), Scalar(0, 0, 255), 2);
+		}
+	}
+
+    IMSHOW(GetFullName(), outputFrame_);
 	output_ = /*HasSuccessor() ? new ImageWrapper(*imageWrapperIn_) :*/ NULL;
 }
